6bd99953d58dbf828d9d2edcbde93b4ccde6a438
Stephan D Ramalho <sramalho@fordham.edu>
2022-12-12 10:55:51 -0500

    added component routes into App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,51 @@
-import './index.css';
-import Test from './components/TestComponent';
+import './index.css'
+import { Routes, Route } from 'react-router-dom'
+import { useEffect, useState } from 'react'
+import { CheckSession } from './services/Auth'
+import Nav from './components/Nav'
+import Home from './Pages/Home'
+import Test from './components/TestComponent'
 
 function App() {
+  const [authenticated, toggleAuthenticated] = useState(false)
+  const [user, setUser] = useState(null)
+
+  const handleLogOut = () => {
+    setUser(null)
+    toggleAuthenticated(false)
+    localStorage.clear()
+  }
+
+  const checkToken = async () => {
+    const user = await CheckSession()
+    setUser(user)
+    toggleAuthenticated(true)
+  }
+
+  useEffect(() => {
+    const token = localStorage.getItem('token')
+    if (token) {
+      checkToken()
+    }
+  })
   return (
     <div>
-      <Test />
+      <div>
+        <header>
+          <Nav
+            authenticated={authenticated}
+            user={user}
+            handleLogOut={handleLogOut}
+          />
+        </header>
+        <main>
+          <Routes>
+            <Route index element={<Home authenticated={authenticated} />} />
+          </Routes>
+        </main>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
