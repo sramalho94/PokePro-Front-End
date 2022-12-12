018a4605391d0a15ddabc9c0d8477a96972c388e
Stephan D Ramalho <sramalho@fordham.edu>
2022-12-12 15:53:53 -0500

    created Trainer component

---
+++ client/src/components/Trainer.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Trainer = ({id, name, image }) => {
+  return (
+    <div className='trainer-card' key={id}>
+      <div className='image-wrapper'>
+        {/* open link here */}
+        <img src={image} alt={`Pic of trainer: ${name}`}/>
+        {/* close link here */}
+      </div>
+      <div className='trainer-info'>
+        <Link to={`trainer_team/${id}`} >
+        <h2>{` Trainer: ${name}`}</h2>
+        </Link>
+      </div>
+    </div>
+  )
+}
+
+export default Trainer
