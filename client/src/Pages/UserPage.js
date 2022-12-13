4cc9e1913129a9ce9837bfbbe0c69bd32a8278d7
Stephan D Ramalho <sramalho@fordham.edu>
2022-12-12 19:37:34 -0500

    created skeleton for user page

---
+++ client/src/Pages/UserPage.js
@@ -0,0 +1,10 @@
+import { useParams } from 'react-router-dom'
+import axios from 'axios'
+
+import React from 'react'
+
+const UserPage = () => {
+  return <div>UserPage</div>
+}
+
+export default UserPage
