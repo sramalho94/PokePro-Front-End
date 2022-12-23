d4aaf5b916b5c58e6137ee747c6308d36f476f9e
Stephan D Ramalho <sramalho@fordham.edu>
2022-12-23 11:05:54 -0500

    added and edited pokemon card

---
+++ src/components/PokemonCard.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const PokemonCard = ({id, name, image, types, sprite}) => {
+  return (
+    <div className='pokemon-card'>
+      <div className='image-wrapper'>
+        <img src={image} className="pokemon-image"/>
+      </div>
+      <div className='pokemon-info'>
+      <div className='pokemon-name-container'>
+        <h2 className='header-text'>{`${name}`}</h2>
+        <div className='type-container'>
+          {types?.map((type)=>(
+            <h3 key={type} className='header-text'>{type}</h3>
+          ))}
+        </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default PokemonCard
