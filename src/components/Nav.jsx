615aceef9f5f4545acd9d08130eba3d0eac28aff
Stephan D Ramalho <sramalho@fordham.edu>
2023-03-09 14:53:50 -0500

    redid styling in Nav component with tailwind

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,42 +1,73 @@
-import { NavLink, Link } from "react-router-dom";
+import { NavLink, Link } from 'react-router-dom'
 import React from 'react'
 
-const Nav = ({ authenticated, user, handleLogOut}) => {
+const Nav = ({ authenticated, user, handleLogOut }) => {
   let authenticatedOptions
-  if (user){
+  if (user) {
     authenticatedOptions = (
-        <nav className="menu_box">
-          <NavLink to = '/' className="nav-link"> Home </NavLink>
-          <NavLink to={`/profile_page/${user.id}`} className="nav-link"> Profile Page</NavLink>
-          <NavLink onClick={handleLogOut} to='/' className="nav-link">Sign Out</NavLink>
-          <NavLink to="/pokemon" className="nav-link">Pokemon</NavLink>
-        </nav>
+      <nav className="flex flex-row align-middle justify-center px-3">
+        <NavLink to="/" className="nav-link px-3">
+          {' '}
+          Home{' '}
+        </NavLink>
+        <NavLink to={`/profile_page/${user.id}`} className="nav-link px-1">
+          {' '}
+          Profile Page
+        </NavLink>
+        <NavLink onClick={handleLogOut} to="/" className="nav-link px-1">
+          Sign Out
+        </NavLink>
+        <NavLink to="/pokemon" className="nav-link px-1">
+          Pokemon
+        </NavLink>
+      </nav>
     )
   }
 
   const publicOptions = (
     <nav>
-      <NavLink to = '/login' className="login-link"> Login </NavLink>
+      <NavLink to="/login" className="login-link">
+        {' '}
+        Login{' '}
+      </NavLink>
     </nav>
   )
-  
+
   return (
-    <header className="nav-container"> 
-      <span id='nav-banner'>
-        <marquee>
-          <div className="marquee-content">
-          <h4 id='nav-banner-text'> Having trouble being the very best? PokePro is here for you</h4>
-          <img src='https://thumbs.gfycat.com/GenerousTimelyBrontosaurus-max-1mb.gif'
-          className="marquee-image" alt="not found"></img>
+    <header className="nav-container">
+      <div className=" bg-gradient-to-l from-red-500 via-black to-white">
+        <div className="marquee-container overflow-hidden relative h-10 w-full">
+          <div className=" flex flex-row marquee-content absolute top-0 left-0 whitespace-nowrap animate-marquee">
+            <h4 className="my-auto text-[#F6CF57]">
+              {' '}
+              Having trouble being the very best? PokePro is here for you
+            </h4>
+            <img
+              src="https://thumbs.gfycat.com/GenerousTimelyBrontosaurus-max-1mb.gif"
+              className="h-fit w-10"
+              alt="not found"
+            ></img>
           </div>
-        </marquee>
-      </span>
-      <div id='nav-content'>
+        </div>
+      </div>
+
+      <div
+        id="nav-content"
+        className="flex flex-row justify-between bg-gradient-to-r from-red-500 via-black to-white text-[#F6CF57]"
+      >
         <Link to={'/'} id="logo-link">
-          <img src="https://www.serebii.net/letsgopikachueevee/pokemon/025-crown.png" alt="website logo" className="logo"/>
+          <img
+            src="/pikachucrown.png"
+            alt="website logo"
+            className="h-10 w-10"
+          />
         </Link>
-        <div id= 'links-container'> 
-          {authenticated && user ? authenticatedOptions: publicOptions }
+        <div
+          id="links-container"
+          className="my-auto
+        "
+        >
+          {authenticated && user ? authenticatedOptions : publicOptions}
         </div>
       </div>
     </header>
