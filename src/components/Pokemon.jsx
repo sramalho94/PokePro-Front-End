ae1ad24d214800214f75b7b22a613c261ffc0187
Stephan D Ramalho <sramalho@fordham.edu>
2023-03-10 14:15:42 -0500

    refactor of component

--- src/components/Pokemon.jsx
+++ src/components/Pokemon.jsx
@@ -1,89 +1,94 @@
 import React from 'react'
 import Client from '../services/api'
 
-const Pokemon = ({id, name, image, types, trainer, sprite, getTeams}) => {
-
-  const handleDelete = async() => {
-    const response = await Client.delete(`https://pokepro-backend.herokuapp.com/api/teams/trainer/${trainer}/pokemon/${id}`)
+const Pokemon = ({ id, name, image, types, trainer, sprite, getTeams }) => {
+  const handleDelete = async () => {
+    const response = await Client.delete(
+      `https://pokepro-backend.herokuapp.com/api/teams/trainer/${trainer}/pokemon/${id}`
+    )
     getTeams()
   }
-  
-  return (
-    <div className='pokemon-card'>
-      <div className='remove-pokemon-button'>
-      <button onClick={handleDelete} className="button"> Remove</button>
-      </div>
-      <div className='pokemon-name-wrapper'>
-      <h2 className='header-text'>{name}</h2>
-      </div>
-      <div className='pokemon-wrapper'>
-        <div className='pokemon-image-wrapper'>
-        <img src={sprite}/>
-        </div>
-        <div className='pokemon-types'>
-        {types?.map((type)=>(
-          <div key={type}>
-          <div className='type-text'>{type}</div>
-          <div> Weak Against:</div>
-          {type === "grass" &&
-            <div className='type-counter-text'> Fire type</div>
-          }
-          {type === "poison" &&
-            <div className='type-counter-text'>Ground type </div>
-          }
-          {type === "normal" &&
-            <div className='type-counter-text'>Fighting type </div>
-          }
-          {type === "fire" &&
-            <div className='type-counter-text'>water type </div>
-          }
-          {type === "water" &&
-            <div className='type-counter-text'>Electric type </div>
-          }
-          {type === "electric" &&
-            <div className='type-counter-text'>Ground type </div>
-          }
-          {type === "ice" &&
-            <div className='type-counter-text'>Fire type </div>
-          }
-          {type === "fighting" &&
-            <div className='type-counter-text'>Fairy type </div>
-          }
-          {type === "ground" &&
-            <div className='type-counter-text'>Grass type </div>
-          }
-          {type === "flying" &&
-            <div className='type-counter-text'>Electric type </div>
-          }
-          {type === "psychic" &&
-            <div className='type-counter-text'>Bug type </div>
-          }
-          {type === "bug" &&
-            <div className='type-counter-text'>Fire type </div>
-          }
-          {type === "rock" &&
-            <div className='type-counter-text'>Water type </div>
-          }
-          {type === "ghost" &&
-            <div className='type-counter-text'>Dark type </div>
-          }
-          {type === "dragon" &&
-            <div className='type-counter-text'>Ice type </div>
-          }
-          {type === "dark" &&
-            <div className='type-counter-text'>Bug type </div>
-          }
-          {type === "steel" &&
-            <div className='type-counter-text'>Ice type </div>
-          }
-          {type === "fairy" &&
-            <div className='type-counter-text'>Poison type </div>
-          }
-          </div>
+
+  const divStyle = {
+    backgroundImage: 'url(/background.png)'
+  }
+  const findWeakness = (types) => {
+    const weaknessTable = {
+      grass: 'fire',
+      poison: 'ground',
+      normal: 'fighting',
+      fire: 'water',
+      water: 'electric',
+      electric: 'ground',
+      ice: 'fighting',
+      ground: 'grass',
+      fighting: 'fairy',
+      flying: 'electric',
+      psychic: 'bug',
+      bug: 'fire',
+      rock: 'water',
+      ghost: 'dark',
+      dragon: 'ice',
+      dark: 'bug',
+      steel: 'ice',
+      fairy: 'steel'
+    }
+    let weaknesses = new Set()
+    for (let type of types) {
+      weaknesses.add(weaknessTable[type])
+    }
+    weaknesses = Array.from(weaknesses)
+
+    return (
+      <div>
+        {weaknesses.map((weakness) => (
+          <p key={weakness}>{weakness}</p>
         ))}
+      </div>
+    )
+  }
+  return (
+    <div
+      id="pokemon-card"
+      className="flex flex-col align-middle p-1 rounded-xl mx-auto"
+    >
+      <div className="pokemon-name-wrapper">
+        <h2 className="header-text">{name}</h2>
+      </div>
+      <div className="pokemon-wrapper">
+        <div className="pokemon-image-wrapper">
+          <img
+            src={sprite}
+            alt={id}
+            className="p-1 rounded-xl"
+            style={divStyle}
+          />
+        </div>
+        <div className="pokemon-types">
+          <p>type:</p>
+          <div
+            className="flex flex-wrap
+          "
+          >
+            {types.map((type) => (
+              <div
+                className={`p-1 text-black rounded-lg border-white bg-${type}`}
+                key={type}
+              >
+                {type}
+              </div>
+            ))}
+          </div>
+          <p>Weak to:</p>
+          {findWeakness(types)}
         </div>
       </div>
-      
+      <div className="remove-pokemon-button">
+        <button onClick={handleDelete} className="button">
+          {' '}
+          Remove
+        </button>
+      </div>
     </div>
   )
 }
