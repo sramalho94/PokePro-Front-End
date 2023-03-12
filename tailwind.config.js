812267a2fcf5df4c8bcd77248014a9488f170b34
Stephan D Ramalho <sramalho@fordham.edu>
2023-03-12 11:38:08 -0400

    changes to color

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,28 @@ const colors = require('tailwindcss/colors')
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extends: {},
+    extends: {
+      backgroundColor: {
+        grass: '#7AC74C',
+        poison: '#A33EA1',
+        normal: '#A8A77A',
+        fire: '#EE8130',
+        water: '#6390F0',
+        electric: '#F7D02C',
+        ice: '#96D9D6',
+        ground: '#E2BF65',
+        fighting: '#C22E28',
+        flying: '#A98FF3',
+        psychic: '#F95587',
+        bug: '#A6B91A',
+        rock: '#B6A136',
+        ghost: '#735797',
+        dragon: '#6F35FC',
+        dark: '#705746',
+        steel: '#B7B7CE',
+        fairy: '#D685AD'
+      }
+    },
     colors: {
       transparent: 'transparent',
       current: 'currentColor',
