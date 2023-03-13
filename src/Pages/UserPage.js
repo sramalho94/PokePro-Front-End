bcca3850d562f71ca3b43c588f4f2d83e0c55158
Stephan D Ramalho <sramalho@fordham.edu>
2023-03-13 15:32:06 -0400

    styling for user page

--- src/Pages/UserPage.js
+++ src/Pages/UserPage.js
@@ -1,6 +1,5 @@
 import { useParams } from 'react-router-dom'
 import { useState, useEffect } from 'react'
-import axios from 'axios'
 import { Link } from 'react-router-dom'
 import React from 'react'
 import Client from '../services/api'
@@ -19,7 +18,7 @@ const UserPage = ({ user, handleLogOut }) => {
       SetUserDetails(response.data)
     }
     getUserDetails()
-  }, [])
+  }, [id])
 
   const handleDelete = async () => {
     const response = await Client.delete(
@@ -29,20 +28,35 @@ const UserPage = ({ user, handleLogOut }) => {
     navigate('/')
   }
 
+  const divStyle = {
+    backgroundImage: 'url(/background.png)',
+    backgroundRepeat: 'no-repeat',
+    backgroundSize: 'cover'
+  }
+
   return (
-    <div className="user-page-container">
-      <div className="user-page-header">
+    <div className="min-h-screen flex flex-col justify-center" style={divStyle}>
+      <div className="text-3xl font-bold text-center align-middle">
         {' '}
         {`Welcome ${userDetails?.userName}`}{' '}
       </div>
-      <div className="user-page-image-wrapper">
-        <img src={userDetails?.image} className="user-page-image" />
-        <Link to={`/edit_info/${id}`}>
-          <button className="button">Edit Info</button>
-          <button onClick={handleDelete} className="button">
-            Delete User
-          </button>
-        </Link>
+      <div className="flex flex-col mx-auto">
+        <img
+          src={userDetails?.image}
+          className="h-48 w-58 rounded-3xl"
+          alt={userDetails.userName}
+        />
+        <div className="flex flex-row justify-center m-3">
+          <Link to={`/edit_info/${id}`}>
+            <button className="p-1 m-1 bg-white rounded-2xl">Edit Info</button>
+            <button
+              onClick={handleDelete}
+              className="m-1 p-1 bg-white rounded-2xl"
+            >
+              Delete User
+            </button>
+          </Link>
+        </div>
       </div>
     </div>
   )
