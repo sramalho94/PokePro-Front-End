105d3dc9165c24660eaeeb681273752b02ce1c64
Stephan D Ramalho <sramalho@fordham.edu>
2023-03-13 17:21:27 -0400

    styling

--- src/Pages/Welcome.js
+++ src/Pages/Welcome.js
@@ -2,31 +2,50 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 const Welcome = () => {
+  const divStyle = {
+    backgroundImage: 'url(/background.png)',
+    backgroundRepeat: 'no-repeat',
+    backgroundSize: 'cover'
+  }
+
   return (
-    <div className="welcome-container">
-      <header className="welcome-header-container">
-        <img
-          src="https://archives.bulbagarden.net/media/upload/7/73/Spr_5b_150.png"
-          alt="website logo"
-          className="welcome-logo"
-        />
-        <div className="welcome-text">
-          <h2>Poke Pro</h2>
-          <h4 className="header-text"> Please follow Community Guidelines </h4>
-          <h3>
-            {' '}
-            Only Change A Trainer's Roster if it is officially changed in game
-          </h3>
-          <h3> Don't be toxic</h3>
-          <h3>
-            {' '}
-            We will be adding an Add Pokemon feature, please only add official
-            pokemon
-          </h3>
-          <div id="welcome-button-container">
-            <Link to="/">
-              <button className="button"> I AGREE </button>
-            </Link>
+    <div
+      className="flex flex-col justify-center align-middle min-h-screen"
+      style={divStyle}
+    >
+      <header className="flex flex-col justify-center">
+        <div className="flex flex-row justify-center">
+          <img
+            src="https://archives.bulbagarden.net/media/upload/7/73/Spr_5b_150.png"
+            alt="website logo"
+            className="h-56 w-56"
+          />
+        </div>
+        <div className="flex flex-row justify-center">
+          <div className="flex flex-col">
+            <h2 className="text-xl font-bold">Poke Pro</h2>
+            <h4 className="text-xl font-bold">
+              {' '}
+              Please follow Community Guidelines{' '}
+            </h4>
+            <h3 className="text-xl">
+              {' '}
+              Only Change A Trainer's Roster if it is officially changed in game
+            </h3>
+            <h3 className="text-xl"> Don't be toxic</h3>
+            <h3 className="text-xl">
+              {' '}
+              We will be adding an Add Pokemon feature, please only add official
+              pokemon
+            </h3>
+            <div className="flex flex-row justify-center">
+              <Link to="/">
+                <button className="p-1 m-1 mx-auto bg-white rounded-lg">
+                  {' '}
+                  I AGREE{' '}
+                </button>
+              </Link>
+            </div>
           </div>
         </div>
       </header>
