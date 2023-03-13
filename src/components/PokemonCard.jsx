a5588663c08b55b0431dc129c2da5098cfcfb717
Stephan D Ramalho <sramalho@fordham.edu>
2023-03-13 17:21:35 -0400

    styling

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,19 +1,21 @@
 import React from 'react'
 
-const PokemonCard = ({id, name, image, types, sprite}) => {
+const PokemonCard = ({ id, name, image, types, sprite }) => {
   return (
-    <div className='pokemon-card'>
-      <div className='image-wrapper'>
-        <img src={image} className="pokemon-image"/>
+    <div className="pokemon-card">
+      <div className="image-wrapper">
+        <img src={sprite} className="pokemon-image" alt={name} />
       </div>
-      <div className='pokemon-info'>
-      <div className='pokemon-name-container'>
-        <h2 className='header-text'>{`${name}`}</h2>
-        <div className='type-container'>
-          {types?.map((type)=>(
-            <h3 key={type} className='header-text'>{type}</h3>
-          ))}
-        </div>
+      <div className="pokemon-info">
+        <div className="pokemon-name-container">
+          <h2 className="header-text">{`${name}`}</h2>
+          <div className="type-container">
+            {types?.map((type) => (
+              <h3 key={type} className="header-text">
+                {type}
+              </h3>
+            ))}
+          </div>
         </div>
       </div>
     </div>
