308f9f5b211fba4e084853f4e7a8d3cfdf35d6af
Stephan D Ramalho <sramalho@fordham.edu>
2023-03-15 09:32:58 -0400

    styling changes

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -2,16 +2,20 @@ import React from 'react'
 
 const PokemonCard = ({ id, name, image, types, sprite }) => {
   return (
-    <div className="pokemon-card">
-      <div className="image-wrapper">
-        <img src={sprite} className="pokemon-image" alt={name} />
+    <div className="flex flex-col justify-center align-middle m-3 bg-purple-400 rounded-lg shadow-xl md:mx-20">
+      <div className="flex flex-row justify-center">
+        <img
+          src={sprite}
+          className="p-1 bg-pink-500 rounded-lg mt-3 shadow-xl"
+          alt={name}
+        />
       </div>
       <div className="pokemon-info">
-        <div className="pokemon-name-container">
-          <h2 className="header-text">{`${name}`}</h2>
-          <div className="type-container">
+        <div className="flex flex-col justify-center pt-2">
+          <h2 className="p-1 font-rubik-puddles text-center">{`${name}`}</h2>
+          <div className="flex flex-row justify-center">
             {types?.map((type) => (
-              <h3 key={type} className="header-text">
+              <h3 key={type} className={`p-1 font-rubik-iso text-${type}`}>
                 {type}
               </h3>
             ))}
